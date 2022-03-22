1e176eaf0c5ee1ad2aae7ba558a4b1bceae7f767
Abel Masila <abel@x2ai.com>
2022-03-22 16:50:59 +0300

    deploy to alchemy

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x2D0055f037591e29881B5597B5917dA661207C68';
--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,21 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/IBrOUyQ8X9xwZ9tw0SXOKn2qwMrH5IlC',
+      accounts: [
+        '58fd6300e32d2ae2b507e781b895e4756a25901312b7311af80b00f0d3bb3888',
+      ],
+    },
+  },
 };
